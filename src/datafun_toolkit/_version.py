1c73bd30e3d84cb5bf8fe7f2b72720faff09ddcd
Denise Case <denisecase@gmail.com>
2026-01-02 10:07:51 -0600

    initial

--- src/datafun_toolkit/_version.py
+++ src/datafun_toolkit/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.2.dev1'
-__version_tuple__ = version_tuple = (0, 0, 2, 'dev1')
+__version__ = version = '0.0.2.dev5'
+__version_tuple__ = version_tuple = (0, 0, 2, 'dev5')
 
-__commit_id__ = commit_id = 'gaa071401e'
+__commit_id__ = commit_id = 'g62e7d7608'
